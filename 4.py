ddd486e8a65cd7de00a8469747f60f10fafb686f
dmitrybuben <dmitry.buben@gmail.com>
2022-04-17 19:06:44 +0300

    4.py

---
+++ 4.py
@@ -0,0 +1,16 @@
+# Показать первую цифру дробной части числа
+n = 101.456
+print(int(n*10)%10)
+
+
+#4. Показать первую цифру дробной части числа
+
+# number = float(input('Введите число: '))
+# number1=str(float(number))
+# number2 = number1.split('.')
+# print(number2)
+# #print(type(number1))
+# number3 = number2[1]
+# #print(number3)
+# number_fin =number3[0]
+# print((number_fin))
