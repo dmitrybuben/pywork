b7abd1173dddba0478ee3e524e68f3235d58ccbc
dmitrybuben <97685811+dmitrybuben@users.noreply.github.com>
2022-05-19 12:09:10 +0300

    41.py

--- 41.py
+++ 41.py
@@ -7,17 +7,17 @@
 # Входные данные: 
 # 12W1B12W3B24W1B14W
 
-init_str = 'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'
+init_str = 'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'        # запись исходной строки в файл 
 data = open('input_41.txt', 'w')
 data.write(init_str)
 data.close
 
-path = 'input_41.txt'
+path = 'input_41.txt'          # чтение строки из файла и запись в переменную
 data = open(path, 'r')
 work_str = data.read()
 data.close
 
-def compress(work_str):
+def compress(work_str):     # сжатие строки
     res_str = ''
     count = 1
     for e in range(len(work_str)-1):
@@ -29,14 +29,14 @@ def compress(work_str):
     res_str += str(count) + work_str[e]
     return res_str
 
-with open('compressed_41.txt', 'w') as data:
+with open('compressed_41.txt', 'w') as data:        # запись сжатой строки в файл
     data.write(compress(work_str))
     
 
-with open('compressed_41.txt','r') as data:
+with open('compressed_41.txt','r') as data:         # считывание сжатой строки в переменную
     comp_str = data.read()
 
-def decompress(comp_str):
+def decompress(comp_str):                           # восстановление сжатой строки
     decomp_str = ''
     char_qty = ''
     for i in range(len(comp_str)):
@@ -47,5 +47,5 @@ def decompress(comp_str):
             char_qty = ''
     return decomp_str
 
-with open('decompressed_41.txt', 'w') as data:
+with open('decompressed_41.txt', 'w') as data:      # запись восстановленной строки в файл
     data.write(decompress(comp_str))
